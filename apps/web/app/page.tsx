5eda757b326accd3e5536ad8e1ebafae0434044d
Alikuxac <alikuxac.dev@gmail.com>
2025-11-10 00:59:40 +0700

    feat: initial commit (AIO-417)

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "../styles/index.module.css";
+
+export default function Web() {
+  return (
+    <div className={styles.container}>
+      <h1>Web</h1>
+    </div>
+  );
+}
