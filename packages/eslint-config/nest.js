6e8a4c0c293d0e7df0b49917e117f5313c5f17d9
Alikuxac <alikuxac.dev@gmail.com>
2025-12-07 17:19:03 +0700

    feat: fix package config for native app

--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -25,7 +25,7 @@ export const nestJsConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-floating-promises": "warn",
-      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/no-unsafe-argument": "off",
     },
   },
 ];
