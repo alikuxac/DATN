e3ab865988ad39f8f6a1bc1a2b97294dc0e34591
Alikuxac <alikuxac.dev@gmail.com>
2026-01-09 15:53:36 +0700

    fix(api): fix user search filter & pagination dto validation

--- apps/api/src/common/pagination/dtos/pagination.list.dto.ts
+++ apps/api/src/common/pagination/dtos/pagination.list.dto.ts
@@ -1,23 +1,30 @@
 import { ApiHideProperty } from '@nestjs/swagger';
+import { Allow } from 'class-validator';
 import { ENUM_PAGINATION_ORDER_DIRECTION_TYPE } from '@repo/shared';
 import { IPaginationOrder } from '../interfaces/pagination.interface';
 
 export class PaginationListDto {
   @ApiHideProperty()
+  @Allow()
   _search: Record<string, any>;
 
   @ApiHideProperty()
+  @Allow()
   _limit: number;
 
   @ApiHideProperty()
+  @Allow()
   _offset: number;
 
   @ApiHideProperty()
+  @Allow()
   _order: IPaginationOrder;
 
   @ApiHideProperty()
+  @Allow()
   _availableOrderBy: string[];
 
   @ApiHideProperty()
+  @Allow()
   _availableOrderDirection: ENUM_PAGINATION_ORDER_DIRECTION_TYPE[];
 }
--- apps/api/src/modules/users/constants/user.list.constant.ts
+++ apps/api/src/modules/users/constants/user.list.constant.ts
@@ -1,4 +1,4 @@
 import { ENUM_USER_STATUS } from '@repo/shared';
 
-export const USER_DEFAULT_AVAILABLE_SEARCH = ['name', 'email'];
+export const USER_DEFAULT_AVAILABLE_SEARCH = ['firstName', 'lastName', 'email'];
 export const USER_DEFAULT_STATUS = Object.values(ENUM_USER_STATUS);
