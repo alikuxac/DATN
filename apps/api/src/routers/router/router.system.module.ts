09d251396d88f83e19bee216952661b4c66366d4
Alikuxac <alikuxac.dev@gmail.com>
2026-01-13 01:33:01 +0700

    fix(api): add authmodule to systemmodule

--- apps/api/src/routers/router/router.system.module.ts
+++ apps/api/src/routers/router/router.system.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { UsersModule } from '@modules/users/users.module';
 import { UserSystemController } from '@modules/users/controllers/user.system.controller';
+import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule],
   controllers: [UserSystemController],
 })
 export class RouterSystemModule { }
