0ceeab761fc10382c43d01780e6e4f7e91dd5e65
Alikuxac <alikuxac.dev@gmail.com>
2026-01-13 02:07:58 +0700

    fix(api): fix missing module in routersystemmodule

--- apps/api/src/routers/router/router.system.module.ts
+++ apps/api/src/routers/router/router.system.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { UsersModule } from '@modules/users/users.module';
 import { UserSystemController } from '@modules/users/controllers/user.system.controller';
+import { ActivityModule } from '@modules/activity/activity.module';
+import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ActivityModule, AuthModule],
   controllers: [UserSystemController],
 })
 export class RouterSystemModule { }
